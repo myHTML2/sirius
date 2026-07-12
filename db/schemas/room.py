0428c68859170248ba18cdd3658ee0f40e394a6a
myHTML <minniahmetovaamira62@gmail.com>
2026-07-12 17:12:58 +0500

    Update room.py

--- db/schemas/room.py
+++ db/schemas/room.py
@@ -1,1 +1,14 @@
+from pydantic import BaseModel
 
+class RoomBase(BaseModel):
+    name: str
+    description: str | None = None
+
+class RoomCreate(RoomBase):
+    pass
+
+class RoomRead(RoomBase):
+    id: int
+
+    class Config:
+        from_attributes = True
