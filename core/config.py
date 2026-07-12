c3d2db42e2997bbb6a4b02c43bf36d5c0e5fcc83
myHTML <minniahmetovaamira62@gmail.com>
2026-07-12 16:56:41 +0500

    Update config.py

--- core/config.py
+++ core/config.py
@@ -1,1 +1,10 @@
+from pydantic_settings import BaseSettings
 
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Booking Service"
+    DATABASE_URL: str = "postgresql://user:password@localhost/dbname"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
