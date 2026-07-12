371ae5538c16b92880ccf9bc78553de3c4fc6ed5
myHTML <minniahmetovaamira62@gmail.com>
2026-07-12 16:37:51 +0500

    Update room.py

--- db/models/room.py
+++ db/models/room.py
@@ -1,1 +1,11 @@
+from sqlalchemy import Column, Integer, String, JSON
+from app.db.schemas.base_class import Base
 
+
+class Room(Base):
+    __tablename__ = "rooms"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    capacity = Column(Integer, nullable=False)
+    equipment = Column(JSON, nullable=False, default=[])
