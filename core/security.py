dde39f807b3a3cd56502f4d59f89307c6b717fa6
myHTML <minniahmetovaamira62@gmail.com>
2026-07-12 17:37:09 +0500

    Update security.py

--- core/security.py
+++ core/security.py
@@ -1,1 +1,7 @@
+import bcrypt
 
+def hash_password(password: str):
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+def verify_password(plain_password: str, hashed_password: str):
+    return bcrypt.checkpw(plain_password.encode(), hashed_password.encode())
