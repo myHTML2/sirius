969d00569b9d1fec2c03e83ca1a8239f93506545
myHTML <minniahmetovaamira62@gmail.com>
2026-07-12 16:50:11 +0500

    Create _init_.py

---
+++ db/schemas/_init_.py
@@ -0,0 +1,13 @@
+from .booking import (
+    BookingBase,
+    BookingCreate,
+    BookingUpdate,
+    BookingRead
+)
+
+__all__ = [
+    "BookingBase",
+    "BookingCreate",
+    "BookingUpdate",
+    "BookingRead"
+]
